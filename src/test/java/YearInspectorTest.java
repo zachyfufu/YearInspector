12a6f745bd36be2fb9f3b5a8fdbca8b794616339
Zachy <zachyfufu@yahoo.com>
2021-11-05 19:59:08 +0100

    fixed test isYearNotDivisibleBy4()

--- src/test/java/YearInspectorTest.java
+++ src/test/java/YearInspectorTest.java
@@ -55,7 +55,7 @@ public class YearInspectorTest {
 
     @Test
     public void isYearNotDivisibleBy4(){
-        assertEquals(false, currentYear.isLeapYear(20));
+        assertEquals(false, currentYear.isLeapYear(2021));
     }
 
     @Test
